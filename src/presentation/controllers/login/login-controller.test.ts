e3f81f842b601f4d081df213eb307f8bf2af4a02
gabriel <gabriel.pr07@hotmail.com>
2022-11-03 13:55:40 -0300

    chore: add unit and integration config files

--- src/presentation/controllers/login/login-controller.test.ts
+++
@@ -1,6 +0,0 @@
-console.log('oi')
-const t4 = (oi: any): any => {
-  return oi
-}
-
-t4('4')
---
+++ src/presentation/controllers/signup/signup-controller.test.ts
@@ -0,0 +1,5 @@
+describe('SignUp Controller', () => {
+  it('Should return 500 if AddAccount throws', () => {
+    expect(2 + 4).toBe(6)
+  })
+})
